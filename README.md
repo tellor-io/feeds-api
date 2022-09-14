7ea757e060995e152d534f47505214bc756dbb9c
oraclown <owen.burton13@gmail.com>
2022-09-14 09:54:33 -0500

    Use functions from telliot-feeds, fix bad status attr

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # feeds-api
 used for decoding/encoding Tellor Protocol data types
+
+## setup
+- install python 3.9.7 (use pyenv)
+- `python -m venv env` (create virtual environment)
+- `source env/bin/activate` (activate virtual env)
+- `pip install -r requirements.txt`
+- `uvicorn main:app --reload` (run server)
+
---
+++ main.py
@@ -0,0 +1,27 @@
+from typing import Union
+
+from fastapi import FastAPI
+from telliot_feeds.utils.decode import decode_query_data, decode_submit_value_bytes
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.post("/decode/query_data/")
+def decode_qd(query_data_str: str):
+    status, query = decode_query_data(query_data_str)
+    if not status.ok:
+        return {"error": status.error}
+    return {"query": str(query)}
+
+@app.post("/decode/submit_value_bytes/")
+def decode_svb(submit_value_bytes_str: str, query_type_str: str):
+    status, value = decode_submit_value_bytes(submit_value_bytes_str, query_type_str)
+    if not status.ok:
+        return {"error": status.error}
+    return {"value": str(value)}
+
