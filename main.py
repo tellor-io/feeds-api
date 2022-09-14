4bdb0a7854ca5545fc6d14d75b85e58910410764
oraclown <owen.burton13@gmail.com>
2022-09-14 14:53:11 -0500

    Prep for Heroku

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from typing import Union
 
 from fastapi import FastAPI
-from telliot_feeds.utils.decode import decode_query_data, decode_submit_value_bytes
+from telliot_feeds.utils.decode import decode_query_data
+# from telliot_feeds.utils.decode import decode_submit_value_bytes
 
 app = FastAPI()
 
@@ -18,10 +19,11 @@ def decode_qd(query_data_str: str):
         return {"error": status.error}
     return {"query": str(query)}
 
-@app.post("/decode/submit_value_bytes/")
-def decode_svb(submit_value_bytes_str: str, query_type_str: str):
-    status, value = decode_submit_value_bytes(submit_value_bytes_str, query_type_str)
-    if not status.ok:
-        return {"error": status.error}
-    return {"value": str(value)}
+# @app.post("/decode/submit_value_bytes/")
+# def decode_svb(submit_value_bytes_str: str, query_type_str: str):
+#     query = query_from_str(query_type_str)
+#     status, value = decode_submit_value_bytes(submit_value_bytes_str, query_type_str)
+#     if not status.ok:
+#         return {"error": status.error}
+#     return {"value": str(value)}
 
